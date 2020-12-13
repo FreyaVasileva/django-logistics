9945d8ecf2a405cf52e7b9c66c78615726194fb2
FreyaVasileva <74960126+FreyaVasileva@users.noreply.github.com>
2020-12-13 11:25:40 +0200

    Added Final Touches

--- consignment/models.py
+++ consignment/models.py
@@ -8,7 +8,7 @@ class Consignment(models.Model):
         ('delivered', 'DELIVERED'),
         ('cancelled', 'CANCELLED'),
     )
-    tracking_number = models.IntegerField(blank=False, unique=True)
+    tracking_number = models.IntegerField(blank=False, unique=True, error_messages={'unique': "already been register"})
     delivery_date = models.DateField(blank=False)
     status = models.CharField(choices=STATUS_CHOICES, max_length=20, default='pending_delivery')
     receiver = models.ForeignKey(User, limit_choices_to={'groups': 1}, on_delete=models.CASCADE)
